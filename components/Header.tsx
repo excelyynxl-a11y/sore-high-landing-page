8263422309ffcd4f18281e56496cd4fe72b29bf7
Excelyynx <elim0079@student.monash.edu>
2026-06-17 11:36:05 +0800

    feat(header): create Header section

---
+++ components/Header.tsx
@@ -0,0 +1,68 @@
+'use client';
+import Link from 'next/link';
+import React from 'react';
+import { navItems } from '@/data/data';
+import { useGSAP, gsap, ScrollTrigger } from '@/lib/gsap-util';
+import { useRef } from 'react';
+
+export default function Header() {
+
+    useGSAP(() => {
+        const showAnim = gsap.timeline({paused: true}).fromTo(
+            '.header', 
+            {
+                yPercent: 0,
+            },
+            { 
+                yPercent: -100, 
+                duration: 0.3,
+                ease: 'power2.inOut',
+            }
+        );
+
+        ScrollTrigger.create({
+            start: 'top top',
+            end: 'max',
+            onUpdate: (self) => {
+                self.direction === 1 ? showAnim.play() : showAnim.reverse();
+            },
+            markers: true,
+        });
+    }, {});
+    
+  return (
+    <header className='header sticky top-0 left-0 bg-white/70 backdrop-blur-md w-full py-4 z-50'>
+        <div className='container flex items-center justify-between'>
+            {/* logo */}
+            <Link
+                href={'/'}
+                className='font-bold text-2xl sm:text-3xl'    
+            >
+                Sore High
+            </Link>
+
+            {/* nav list */}
+            <nav className='flex items-center gap-5'>
+                <ul className='flex flex-col sm:flex-row sm:items-center sm:gap-5'>
+                    {navItems.map((item) => (
+                        <li key={item.id}>
+                            <Link 
+                                href={item.href}
+                                className='uppercase font-medium hover:opacity-75 transition-opacity'    
+                            >
+                                {item.label}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+                <button 
+                    className='bg-neutral-900 text-white uppercase px-5 py-3 rounded-lg hover:opacity-85 transition-opacity hidden md:block'
+                    
+                >
+                    Stalk Us 
+                </button>
+            </nav>
+        </div>
+    </header>
+  )
+}
