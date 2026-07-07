b3d2ed0aba9f69cd30436ce17b71d4a5a7dd5fc7
Excelyynx <elim0079@student.monash.edu>
2026-07-07 13:06:31 +0800

    feat(footer): Footer section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,83 @@
-"use client";
-import React from 'react'
+  "use client";
+import { excelContacts } from '@/data/data';
+  import React from 'react'
 
-export default function Footer() {
-  return (
-    <div>Footer</div>
-  )
-}
+  export default function Footer() {
+    return (
+      <footer className="bg-neutral-900 text-white h-[70vh] bottom-0 w-full">
+        <div className="container py-8 flex flex-wrap flex-col justify-center min-h-full">
+          {/* footer list */}
+          <div className="flex flex-wrap items-center gap-7 sm:gap-14 lg:gap-20">
+            {/* links */}
+            <div>
+              {/* links wrapper */}
+              <div className="flex gap-2 mb-3 sm:gap-5">
+                {/* links */}
+                <div className="grid gap-1">
+                  {excelContacts.slice(0, 3).map((contact) => (
+                    <a 
+                      href={contact.link} 
+                      className="uppercase hover:underline" 
+                      key={contact.id}
+                    >
+                      {contact.name}
+                    </a>
+                  ))}
+                </div>
+                <div className="grid gap-1">
+                  {excelContacts.slice(3, 6).map((contact) => (
+                    <a 
+                      href={contact.link} 
+                      className="uppercase hover:underline" 
+                      key={contact.id}
+                    >
+                      {contact.name}
+                    </a>
+                  ))}
+                </div>
+              </div>
+
+              {/* text */}
+              <p className="max-w-xs text-sm opacity-80 mb-3">
+                  We design and build thoughtful digital experiences focused on clarity, 
+                  performance and long-term impact.
+              </p>
+
+              {/* text */}
+              <p className="">
+                &copy; Excelyynx 2026. All right reserved.
+              </p>
+            </div>
+
+            {/* text */}
+            <div className="flex-1">
+              {/* text */}
+              <h3 className="text-4xl font-medium uppercase md:text-6xl lg:text-7xl xl:text-8xl">
+                  let's chat 
+              </h3>
+              {/* text */}
+              <p className="max-w-md mt-2 opacity-80">
+                Interested to kick start a colloboration with us?
+                We're always open to meaningful conversation.
+              </p>
+              
+              {/* text */}
+              <p className="">
+                  privacy policy
+              </p>
+              {/* text wrapper */}
+              <div className="mt-4 text-sm">
+                <p>
+                  Email: {"  "} 
+                  <a href="mailto:excelyynxl@gmail.com" className="hover:underline">
+                    excelyynxl@gmail.com
+                  </a>
+                </p>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    )
+  }
