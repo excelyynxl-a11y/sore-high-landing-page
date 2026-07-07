d2c761708f11fe325dc2a719e5b64af51524b923
Excelyynx <elim0079@student.monash.edu>
2026-07-07 19:44:20 +0800

    feat(scroll): implement handleScroll() effect

--- components/Header.tsx
+++ components/Header.tsx
@@ -29,34 +29,49 @@ export default function Header() {
         });
     }, {});
 
+    const handleScroll = (elementId: string) => {
+        if (!elementId) {
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+            return;
+        }
+
+        const section = document.getElementById(elementId);
+        if (section) {
+            section.scrollIntoView({ behavior: 'smooth' });
+        }
+    }
+
   return (
-    <header className='header sticky top-0 left-0 bg-white/70 backdrop-blur-md w-full py-4 z-50 header'>
+    <header 
+        className='header sticky top-0 left-0 bg-white/70 backdrop-blur-md w-full py-4 z-50 header'
+        id="header"
+    >
         <div className='container flex items-center justify-between'>
             {/* logo */}
-            <Link
-                href={'/'}
+            <button
+                onClick={() => handleScroll("")}
                 className='font-bold text-2xl sm:text-3xl'    
             >
                 Sore High
-            </Link>
+            </button>
 
             {/* nav list */}
             <nav className='flex items-center gap-5'>
                 <ul className='flex flex-col sm:flex-row sm:items-center sm:gap-5'>
                     {navItems.map((item) => (
                         <li key={item.id}>
-                            <Link 
-                                href={item.href}
+                            <button 
+                                onClick={() => handleScroll(item.sectionId)}
                                 className='uppercase font-medium hover:opacity-75 transition-opacity'    
                             >
                                 {item.label}
-                            </Link>
+                            </button>
                         </li>
                     ))}
                 </ul>
                 <button 
                     className='bg-neutral-900 text-white uppercase px-5 py-3 rounded-lg hover:opacity-85 transition-opacity hidden md:block'
-                    
+                    onClick={() => handleScroll('contact')}
                 >
                     Stalk Us 
                 </button>
