952400909c0f5a595a421260b30f009141b2a65f
Excelyynx <elim0079@student.monash.edu>
2026-07-08 09:31:12 +0800

    feat(footer): Footer section, unstyled

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { gsap, useGSAP, SplitText } from '@/lib/gsap-util';
-import Image from 'next/image';
 import { useRef } from 'react';
+import { Camera, MailIcon, MessageCircle, PhoneCall } from 'lucide-react';
 
 export default function Contact() {
 
@@ -38,21 +38,25 @@ export default function Contact() {
   }, { scope: containerRef }); 
 
   return (
-    <section className="section" ref={containerRef}>
+    <section 
+      className="py-20 mb-[10vh] section" 
+      ref={containerRef}
+      id="contact"
+    >
       <div className="container">
         {/* title */}
         <div className="sm:mx-auto contact-wrapper">
           {/* text wrapper */}
           <div className="flex gap-5 items-end">
             <h2 className="section-title contact-text">
-              Contact 
+              Enough Talk,
             </h2>
             <p className="max-w-60 uppercase font-medium hidden md:block sizechart-text">
               Place your orders, DMs, collaboration or borak
             </p>
           </div>
           <h2 className="section-title contact-text">
-            Us
+            Contact Us
           </h2>
 
           {/* small text */}
@@ -61,7 +65,84 @@ export default function Contact() {
           </p>
 
           <div>
-            
+            <div className="space-y-1.5 pb-5 pt-5">
+              <div className="flex space-x-5">
+                <Camera />
+                <div className="">
+                  <h3 className="text-xl uppercase font-medium text">
+                      Instagram
+                  </h3>
+                  <a 
+                    className="max-w-md text hover:underline"
+                    href="https://www.instagram.com/sorehigh__/"
+                    target="_blank"
+                  >
+                      @sorehigh__
+                  </a>
+                  <a 
+                      className="max-w-md text hover:underline"
+                      href="https://www.instagram.com/aziz_moham3d/"
+                      target="_blank"
+                  >
+                      @aziz_moham3d
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="space-y-1.5 pb-5 pt-5">
+              <div className="flex space-x-5">
+                <MessageCircle />
+                <div className="">
+                  <h3 className="text-xl uppercase font-medium text">
+                      Whatsapp
+                  </h3>
+                  <a 
+                    className="max-w-md text hover:underline"
+                    href="https://wa.me/60193709781"
+                    target="_blank"
+                  >
+                      +6019-370-9781 (Aziz)
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="space-y-1.5 pb-5 pt-5">
+              <div className="flex space-x-5">
+                <MailIcon />
+                <div className="">
+                  <h3 className="text-xl uppercase font-medium text">
+                      Email
+                  </h3>
+                  <a 
+                    className="max-w-md text hover:underline"
+                    href="mailto:aziz_email@gmail.com"
+                    target="_blank"
+                  >
+                      aziz_email@gmail.com
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="space-y-1.5 pb-5 pt-5">
+              <div className="flex space-x-5">
+                <PhoneCall />
+                <div className="">
+                  <h3 className="text-xl uppercase font-medium text">
+                      Phone
+                  </h3>
+                  <a 
+                    className="max-w-md text hover:underline"
+                    href="tel:+601128805868"
+                    target="_blank"
+                  >
+                    +6019-370-9781
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div> 
